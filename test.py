bce761983144114d3189d7948e0a960555c02ef6
gransplash <gransplash@gmail.com>
2024-07-04 18:24:07 +0300

    Project name

--- test.py
+++ test.py
@@ -4,9 +4,6 @@ name = input('What\'s your name: ')
 print('Hello, ', name,'!')
 print('We learn git')
 print('We learn github')
-<<<<<<< HEAD
 print('Our project: Git tutorial')
-=======
 
 print("Today: ", datetime.today().strftime('%d/%m/%Y'))
->>>>>>> refs/remotes/origin/main
