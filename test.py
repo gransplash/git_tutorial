3a052d1bf9faa2cb7d02c52f998bb71c3b4ba65d
gransplash <gransplash@gmail.com>
2024-07-04 18:57:09 +0300

    Update test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import datetime
 
 name = input('What\'s your name: ')
 print('Hello, ', name,'!')
-print('We learn git')
-print('We learn github')
+print('We learn Git and Github')
+rint('Our project: Git tutorial')
 
 print("Today: ", datetime.today().strftime('%d/%m/%Y'))
